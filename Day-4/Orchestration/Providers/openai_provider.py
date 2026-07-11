f942c14d93afa96bc6402b717c9ec282276f46ef
DakshSahni4 <dakshsahni2004@gmail.com>
2026-07-12 01:39:08 +0530

    Add OpenAI api

--- Day-4/Orchestration/Providers/openai_provider.py
+++ Day-4/Orchestration/Providers/openai_provider.py
@@ -1,36 +1,60 @@
 import time
 from Providers.base_provider import BaseAIProvider
 
+from openai import OpenAI
+
+from dotenv import load_dotenv
+
+import os 
+
+load_dotenv()
+
+
 
 class OpenAIProvider(BaseAIProvider):
+    def __init__(self):
+        self.client = OpenAI(
+            api_key=os.getenv("OPENAI_API_KEY")
+        )
 
     def generate_text(self, prompt: str):
         start = time.time()
 
+        response = self.client.responses.create(
+            model = "gpt-5",
+            input = prompt
+        )
         return {
             "success": True,
             "provider": "OpenAI",
-            "output": f"Generated using OpenAI: {prompt}",
+            "output": response.output_text,
             "execution_time": round(time.time() - start, 4)
         }
 
     def summarize(self, text: str):
         start = time.time()
-
+        response = self.client.responses.create(
+            model ="gpt-5",
+            input = f"Summarize this Text {text}"
+        )
         return {
             "success": True,
             "provider": "OpenAI",
-            "output": "Summary generated using OpenAI",
+            "output": response.output_text,
             "execution_time": round(time.time() - start, 4)
         }
 
     def classify(self, text: str):
         start = time.time()
 
+        response = self.client.responses.create(
+            model = "gpt-5",
+            input = f" Classify if this text is Negative, Positive or Neutral {text}"
+        )
         return {
             "success": True,
             "provider": "OpenAI",
-            "output": "Classification using OpenAI",
+            "output": response.output_text,
             "execution_time": round(time.time() - start, 4)
         }
 
