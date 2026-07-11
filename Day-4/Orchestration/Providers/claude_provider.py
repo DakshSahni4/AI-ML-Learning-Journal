b216cac223625515633f4aadb06b1c1baf6ac19f
DakshSahni4 <dakshsahni2004@gmail.com>
2026-07-12 01:54:44 +0530

    Add the claude api

--- Day-4/Orchestration/Providers/claude_provider.py
+++ Day-4/Orchestration/Providers/claude_provider.py
@@ -1,36 +1,73 @@
 import time
 from Providers.base_provider import BaseAIProvider
 
+from anthropic import Anthropic
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
 
 class ClaudeProvider(BaseAIProvider):
+    def __init__(self):
+        self.client = Anthropic(
+            api_key = os.getenv("ANTHROPIC_API_KEY")
+        )
 
     def generate_text(self, prompt: str):
         start = time.time()
 
+        response = self.client.messages.create(
+            model = "claude-sonnet-4-20250514",
+            max_tokens = 100,
+            messages = [
+                {
+                    "role":"user",
+                    "content" : prompt
+                }
+            ]
+        )
         return {
             "success": True,
             "provider": "Claude",
-            "output": f"Generated using Claude: {prompt}",
+            "output": response.content[0].text,
             "execution_time": round(time.time() - start, 4)
         }
 
     def summarize(self, text: str):
         start = time.time()
 
+        response = self.client.messages.create(
+            model = "claude-haiku-4-5-20251001",
+            max_tokens = 100,
+            messages = [
+                {
+                    "role": "user",
+                    "content": f" Summarize the following text : {text}"
+                }
+            ]
+        )
         return {
             "success": True,
             "provider": "Claude",
-            "output": "Summary generated using Claude",
+            "output": response.content[0].text,
             "execution_time": round(time.time() - start, 4)
         }
 
     def classify(self, text: str):
         start = time.time()
         
+        response = self.client.messages.create(
+            model = "claude-haiku-4-5-20251001",
+            max_tokens = 100,
+            messages=[{
+                "role":"user",
+                "content": f" Classify the following text as Negative , Postive or Neutral the text is {text}"
+            }]
+        )
         return {
             "success": True,
             "provider": "Claude",
-            "output": "Classification using Claude",
+            "output": response.content[0].text,
             "execution_time": round(time.time() - start, 4)
         }
 
